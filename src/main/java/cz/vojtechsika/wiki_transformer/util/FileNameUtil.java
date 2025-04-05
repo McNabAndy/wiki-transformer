3e91e2936c1a8d3497109587d6f395c6933bb0d4
Vojtěch Šika <vojtechsika@gmail.com>
2025-04-05 14:00:46 +0200

    create utility method to generate unique suffix

--- src/main/java/cz/vojtechsika/wiki_transformer/util/FileNameUtil.java
+++ src/main/java/cz/vojtechsika/wiki_transformer/util/FileNameUtil.java
@@ -1,20 +1,23 @@
 package cz.vojtechsika.wiki_transformer.util;
 
 import java.text.Normalizer;
+import java.util.UUID;
 
 /**
- *
+ *  Utility class providing functionality for sanitizing file names to make them safe for use
+ *  in file systems. This includes normalization, removal of diacritics, replacement of forbidden
+ *  characters, and cleanup of redundant underscores.
  */
 
 public class FileNameUtil {
 
     /**
-     *
-     * @param fileName
-     * @return
+     * Converts a given file name string into a safe format for file system usage
+     * @param fileName  the original file name to be sanitized
+     * @return a sanitized version of the file name
      */
     public static String sanitizeFileName(String fileName) {
-        // Text to lower case and trim white spaces on the start and the end
+        // Text trim white spaces on the start and the end
         fileName = fileName.trim();
 
         // Normalize the string to decompose characters with diacritics ( á → a + ́)
@@ -31,5 +34,19 @@ public class FileNameUtil {
                 .replaceAll("_+", "_")   // Collapse multiple consecutive underscores into one
                 .replaceAll("^_+|_+$", "");  // Remove leading and trailing underscores
         return clearFileName;
+    }
+
+    /**
+     * Generates a unique suffix string based on a random UUID.
+     * @return unique suffix string without dashes
+     */
+    public static String createUniqueSuffix(){
+        // Generate random 128-bit long number in hex characters separated by “-“
+        String uniqueSuffix = UUID.randomUUID().toString();
+
+        // Remove underscores
+        uniqueSuffix = uniqueSuffix.replaceAll("-", "");
+
+        return uniqueSuffix;
     }
 }
